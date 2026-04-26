13d7ed05539353e3e9a769b92e2c7f250ed4798f
Administrator <administrator@local.com>
2026-04-26 13:47:59 +0300

    v2.7.4: Versiyon güncellemesi

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -156,7 +156,7 @@ const SplashScreen = ({ finishLoading }) => {
             letterSpacing: '1.2px',
             textShadow: '0 2px 4px rgba(0,0,0,0.3)'
           }}>
-            v2.7.3 <span style={{ color: 'white', opacity: 0.7, fontWeight: 700 }}>PRIME STABLE</span>
+            v2.7.4 <span style={{ color: 'white', opacity: 0.7, fontWeight: 700 }}>PRIME STABLE</span>
           </span>
         </div>
         <span style={{
